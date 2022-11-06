281d9ce28d9f182b000dfbaca68e4d676e8c0e64
Bharat <bharatrav8077@gmail.com>
2022-11-06 15:29:47 +0530

    Assign Cookies added

--- src/GreedyAgorithms/Leetcode/AssignCookies.java
+++ src/GreedyAgorithms/Leetcode/AssignCookies.java
@@ -1,4 +1,22 @@
 package GreedyAgorithms.Leetcode;
 
+import java.util.Arrays;
+
 public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        int greedFactor[] = g;
+        int cookieSize[] = s;
+
+        Arrays.sort(greedFactor);
+        Arrays.sort(cookieSize);
+
+        int happyChild =0;       //initially
+
+        for (int cookieIndex =0; happyChild< greedFactor.length && cookieIndex<cookieSize.length;cookieIndex++) {
+            if (cookieSize[cookieIndex]>=greedFactor[happyChild]) {
+                happyChild++;
+            }
+        }
+        return happyChild;
+    }
 }
