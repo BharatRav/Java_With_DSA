243e27854e480906326c76716db538608ff2d15d
Bharat Rav <bharatrav0101@gmail.com>
2022-11-12 04:44:59 +0530

    SameTree Added

---
+++ src/Tree/BinaryTree/Leetcode/SameTree.java
@@ -0,0 +1,40 @@
+package Tree.BinaryTree.Leetcode;
+//Leetcode Link: https://leetcode.com/problems/same-tree/description/
+public class SameTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //if both false obviously true
+        if (p == null && q == null) {
+            return true;
+        }
+
+        // if both are not true and anyone of them null it means it's false
+        if (p == null || q == null) {
+            return false;
+        }
+
+        //if both have same value
+        if (p.val == q.val) {
+            boolean leftStatus = isSameTree(p.left,q.left);
+            boolean rightStatus = isSameTree(p.right,q.right);
+
+            return (leftStatus && rightStatus);
+        }
+
+        //if both not same vale but nodes are equal then also it is false
+        return false;
+    }
+
+}
