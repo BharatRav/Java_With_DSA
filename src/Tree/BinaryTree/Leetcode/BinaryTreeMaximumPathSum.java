c6c451907795176cea6c003a4f60b2ea91d7c80b
Bharat Rav <bharatrav0101@gmail.com>
2022-11-11 18:36:14 +0530

    Binary Tree Maximum Path Sum Hard Question Added

---
+++ src/Tree/BinaryTree/Leetcode/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,40 @@
+package Tree.BinaryTree.Leetcode;
+
+import org.w3c.dom.Node;
+
+public class BinaryTreeMaximumPathSum {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    int maxValue;
+
+    private  int getMax(TreeNode currentNode) {
+        int NodeMax=0;
+        if(currentNode!= null) {
+            int leftMax = getMax(currentNode.left);
+            int rightMax = getMax(currentNode.right);
+
+            NodeMax = Math.max(leftMax, rightMax)+currentNode.val;
+            maxValue = Math.max((leftMax+rightMax+currentNode.val),maxValue);
+        }
+        NodeMax = Math.max(NodeMax,0);
+        return NodeMax;
+    }
+
+    public int maxPathSum(TreeNode root) {
+        maxValue =root.val;
+
+        getMax(root);
+
+        return maxValue;
+    }
+}
