dd96167d4ec506b1b7e8445f67257560c29f95d1
Bharat Rav <bharatrav0101@gmail.com>
2022-11-16 04:28:26 +0530

    Chunk Sort Added Right Solution

--- src/GreedyAgorithms/Question/ChunkSort.java
+++ src/GreedyAgorithms/Question/ChunkSort.java
@@ -1,26 +1,38 @@
 package GreedyAgorithms.Question;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChunkSort {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        // Your code here
         Scanner sc = new Scanner(System.in);
-        int n =  sc.nextInt();
+        int n = sc.nextInt();
         int[] arr = new int[n];
-
-        for (int i=0;i<n;i++) {
-            arr[i] =sc.nextInt();
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
         }
+        System.out.println(maxChunksToSorted(arr));
+    }
+    public static int maxChunksToSorted(int[] arr) {
 
-        int result =0;
-        int key =arr[0];
-        for (int i=0;i<n;i++) {
-            if(key>arr[i]) {
-                continue;
+        int n = arr.length;
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+        int[] unsorted = arr.clone();
+
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            sum1+=unsorted[i];
+            sum2+=sorted[i];
+            if(sum1 == sum2){
+                result++;
             }
-            result++;
-            key =arr[i];
         }
-        System.out.println(result);
+        return result;
     }
 }
