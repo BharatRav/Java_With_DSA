3d5cb8f9631a2f83300be8086e7eb027b7707ae0
Bharat Rav <bharatrav0101@gmail.com>
2022-12-09 23:33:53 +0530

    Leetcode 09 dec problem Added Very importan question

---
+++ src/Tree/BinaryTree/Leetcode/MaximumDifferenceBetweenNodeAndAncestor.java
@@ -0,0 +1,28 @@
+package Tree.BinaryTree.Leetcode;
+//Leetcode Link:- https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+
+public class MaximumDifferenceBetweenNodeAndAncestor {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxAncestorDiff(TreeNode root) {
+        return DFS(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private static  int DFS (TreeNode root, int max, int min) {
+        if (root == null) return 0;
+
+        max = Math.max(max,root.val);
+        min = Math.min(min, root.val);
+        return root.left == root.right ? max-min:Math.max(DFS(root.left, max, min), DFS(root.right, max, min));
+    }
+}
