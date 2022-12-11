ab8185f46ac2da8e14d5b8016a7e1f9eb4bc2a11
Bharat Rav <bharatrav0101@gmail.com>
2022-12-11 15:27:54 +0530

    Longest Palindromic String must solve

---
+++ src/String/Leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+package String.Leetcode;
+//Leetcode Link:-https://leetcode.com/problems/longest-palindromic-substring/description/
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() ==0) return "";
+        int start=0, end =0;
+        for (int i=0;i<s.length();i++) {
+            int len1 =expandAroundCenter(s, i, i);  //odd length
+            int len2 =expandAroundCenter(s, i, i + 1);//Even length
+            int len =Math.max(len1,len2);
+
+            if(len>end-start) {
+                start = i -(len-1)/2;
+                end = i+ len/2;
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right-left-1;
+    }
+}
+/*
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+ */
