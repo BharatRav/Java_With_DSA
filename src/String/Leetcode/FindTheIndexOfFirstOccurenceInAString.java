e4f8b0b8cb27d7b38810d98649ba98c83b4511d6
Bharat Rav <bharatrav0101@gmail.com>
2022-12-14 23:21:56 +0530

    added

---
+++ src/String/Leetcode/FindTheIndexOfFirstOccurenceInAString.java
@@ -0,0 +1,7 @@
+package String.Leetcode;
+//Leetcode Link:-https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+public class FindTheIndexOfFirstOccurenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
