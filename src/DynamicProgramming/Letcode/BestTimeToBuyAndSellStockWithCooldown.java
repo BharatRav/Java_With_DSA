a3633cdf35f14a9b0c32797df3963329384bf85e
Bharat Rav <bharatrav8077@gmail.com>
2022-12-23 22:47:36 +0530

    without dp array creating 7 line code

---
+++ src/DynamicProgramming/Letcode/BestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,13 @@
+package DynamicProgramming.Letcode;
+//Leetcode Link:-https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+public class BestTimeToBuyAndSellStockWithCooldown {
+    public int maxProfit(int[] prices) {
+        int profit1=0, profit2=0;
+        for(int i=1; i<prices.length; i++){
+            int copy=profit1;
+            profit1=Math.max(profit1+prices[i]-prices[i-1], profit2);
+            profit2=Math.max(copy, profit2);
+        }
+        return Math.max(profit1, profit2);
+    }
+}
