897f11df232343d529fa9062408402b709cf045d
Bharat Rav <bharatrav8077@gmail.com>
2022-12-24 22:53:12 +0530

    must draw the diagram

---
+++ src/DynamicProgramming/Letcode/DominoAndTrominoTiling.java
@@ -0,0 +1,15 @@
+package DynamicProgramming.Letcode;
+//Leetcode Link:-https://leetcode.com/problems/domino-and-tromino-tiling/description/
+public class DominoAndTrominoTiling {
+    private static final int MOD = 1000000007;
+    public int numTilings(int N) {
+        if (N == 1) return 1;
+        if (N == 2) return 2;
+        long[] dp = new long[N + 1];
+        dp[0] = 1; dp[1] = 1; dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % MOD;
+        }
+        return (int) dp[N];
+    }
+}
