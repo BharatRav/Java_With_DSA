2acf5320d712590523272443ce9af1a0e02641f1
Bharat Rav <bharatrav8077@gmail.com>
2022-12-28 20:16:10 +0530

    Heap question added

---
+++ src/Heap/Leetcode/Heap.java
@@ -0,0 +1,22 @@
+package Heap.Leetcode;
+
+import java.util.PriorityQueue;
+
+//Leetcode Link-https://leetcode.com/problems/remove-stones-to-minimize-the-total/description/
+public class Heap {
+    public int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> b-a);
+        int res = 0;
+        for (int a: piles) {
+            heap.add(a);
+            res+=a;
+        }
+        while (k-->0){
+            int a = heap.poll();
+            heap.add(a-a/2);
+            res-=a/2;
+        }
+        return res;
+        //Tn = O(nlogn);
+    }
+}
