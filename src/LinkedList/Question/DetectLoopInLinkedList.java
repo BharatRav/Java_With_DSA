5ab33db8dea154889f05402336c141fa4526a3c7
Bharat Rav <bharatrav8077@gmail.com>
2023-01-17 23:52:10 +0530

    added

---
+++ src/LinkedList/Question/DetectLoopInLinkedList.java
@@ -0,0 +1,25 @@
+package LinkedList.Question;
+public class DetectLoopInLinkedList {
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    public static boolean detectLoop(Node head){
+        if(head == null || head.next==null) {
+            return false;
+        }
+
+        Node slow=head,fast = head;
+
+        while(fast!=null && fast.next!=null) {
+            slow= slow.next;
+            fast=fast.next.next;
+            if(slow==fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
