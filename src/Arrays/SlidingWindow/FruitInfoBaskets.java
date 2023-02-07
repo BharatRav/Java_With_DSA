a07e3dfc20c9fc5ef32b08a94dd1bbd9c38336bf
Bharat Rav <bharatrav8077@gmail.com>
2023-02-08 00:11:59 +0530

    added

---
+++ src/Arrays/SlidingWindow/FruitInfoBaskets.java
@@ -0,0 +1,21 @@
+package Arrays.SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+//Leetcode Link:-https://leetcode.com/problems/fruit-into-baskets/description/
+
+
+public class FruitInfoBaskets {
+    public int totalFruit(int[] tree) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int i = 0, j;
+        for (j = 0; j < tree.length; ++j) {
+            count.put(tree[j], count.getOrDefault(tree[j], 0) + 1);
+            if (count.size() > 2) {
+                count.put(tree[i], count.get(tree[i]) - 1);
+                count.remove(tree[i++], 0);
+            }
+        }
+        return j - i;
+    }
+}
