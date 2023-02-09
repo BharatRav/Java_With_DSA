210869a4c4abf83d1ff372eb1f309318f0bbf8c1
Bharat Rav <bharatrav8077@gmail.com>
2023-02-09 20:43:36 +0530

    smal contribution to februry placecom quesion solving

---
+++ src/Contest_Fabruary_placeCom/Move_Zeros.java
@@ -0,0 +1,41 @@
+package Contest_Fabruary_placeCom;
+
+import java.util.Scanner;
+
+public class Move_Zeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();;
+        int[] arr = new int[n];
+
+        for (int i=0;i<n;i++) {
+            arr[i]= sc.nextInt();
+        }
+
+        int start=0;
+        int end=n-1;
+
+        while (start<end){
+            while ( end > start && arr[end]==0) {
+                end--;
+            }
+
+            while ( end > start && arr[start]!=0) {
+                start++;
+            }
+
+            if (start<end) {
+                int temp= arr[start];
+                arr[start]=arr[end];
+                arr[end]=arr[start];
+                start++;
+                end--;
+            }
+        }
+
+        for (int val:arr) {
+            System.out.print(val+" ");
+        }
+
+    }
+}
