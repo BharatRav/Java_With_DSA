b8517077b5ca599bca4c55ed2f0dece4db6f6022
Bharat Rav <bharatrav8077@gmail.com>
2023-03-20 22:49:19 +0530

    added a very good approah

---
+++ src/GreedyAgorithms/Leetcode/CanPlaceFlowers.java
@@ -0,0 +1,24 @@
+package GreedyAgorithms.Leetcode;
+
+public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        for (int i=0;i<flowerbed.length;i++) {
+            if(flowerbed[i]==0) {
+
+                int next= (i==flowerbed.length-1)?0:flowerbed[i+1];
+                int prev= (i==0)?0:flowerbed[i-1];
+
+                if(next==0 && prev==0) {
+                    flowerbed[i]=1;
+                    n--;
+                }
+            }
+        }
+
+        if(n>0) {
+            return false;
+        }
+        return true;
+    }
+}
