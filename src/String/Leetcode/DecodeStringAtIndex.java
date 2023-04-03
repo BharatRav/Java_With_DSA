82beffe0535f16eed59f62a560ea5edbd03a67d4
Bharat Rav <bharatrav8077@gmail.com>
2023-04-03 18:59:35 +0530

    added

---
+++ src/String/Leetcode/DecodeStringAtIndex.java
@@ -0,0 +1,34 @@
+package String.Leetcode;
+//Link:-https://leetcode.com/problems/decoded-string-at-index/description/
+public class DecodeStringAtIndex {
+    public String decodeAtIndex(String S, int K) {
+        long curLength = 0;
+        char c = 0;
+
+        for (int i = 0; i < S.length(); i++) {
+            c = S.charAt(i);
+            if (Character.isDigit(c)) { // a1
+                curLength *= c - '0';
+            }
+            else { // b1
+                curLength++;
+            }
+        }
+
+        for (int i = S.length() - 1; i >= 0; i--) {
+            c = S.charAt(i);
+            if (Character.isDigit(c)) { // a2
+                curLength /= c - '0';
+                K %= curLength;
+            }
+            else { // b2
+                if (K == 0 || K == curLength) {
+                    return "" + c;
+                }
+                curLength--;
+            }
+        }
+
+        throw null;
+    }
+}
