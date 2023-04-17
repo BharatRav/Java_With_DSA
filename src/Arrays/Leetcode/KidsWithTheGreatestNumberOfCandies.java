c5dc3160f5456a03a96ff907615c8e4e3b76bcb2
Bharat Rav <bharatrav8077@gmail.com>
2023-04-17 23:48:04 +0530

    added

--- src/Arrays/Leetcode/KidsWithTheGreatestNumberOfCandies.java
+++ src/Arrays/Leetcode/KidsWithTheGreatestNumberOfCandies.java
@@ -1,2 +1,25 @@
-package Arrays.Leetcode;public class KidsWithTheGreatestNumberOfCandies {
+package Arrays.Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxi =Integer.MIN_VALUE;
+
+        for (int data:candies) {
+            maxi = Math.max(maxi, data);
+        }
+
+        ArrayList<Boolean> result = new ArrayList<>(candies.length);
+
+        for (int candy: candies) {
+
+            result.add(candy+extraCandies >=maxi);
+
+        }
+
+        return result;
+
+    }
 }
