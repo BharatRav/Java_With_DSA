d47ad2b07cb42914819cfe7f5a939d7c805b30bd
Bharat Rav <bharatrav8077@gmail.com>
2023-04-18 22:37:54 +0530

    added

--- src/String/Leetcode/MergeStringAlternatively.java
+++ src/String/Leetcode/MergeStringAlternatively.java
@@ -1,2 +1,31 @@
-package String.Leetcode;public class MergeStringAlternatively {
+package String.Leetcode;
+
+public class MergeStringAlternatively {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+
+        int len_word1 =word1.length();
+        int len_word2 =word2.length();
+
+        int i=0,j=0;
+
+        while(i<len_word1 && j<len_word2) {
+            if (i<=j) {
+                sb.append(word1.charAt(i++));
+            } else {
+                sb.append(word2.charAt(j++));
+            }
+        }
+
+        if(i<len_word1) {
+            sb.append(word1.substring(i));
+        }
+
+        if (j<len_word2) {
+            sb.append(word2.substring(j));
+        }
+
+
+        return sb.toString();
+    }
 }
